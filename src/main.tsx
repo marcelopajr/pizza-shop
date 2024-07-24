5fa17c525c564695e15a70c04b999a907d2c3885
Marcelo Pereira <marcelo96jr@gmail.com>
2024-07-24 15:30:23 -0300

    chore: add favicon and fix import on main document

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { enableMSW } from './api/mocks/index.ts'
-import { App } from './app.tsx'
+import { App } from './App.tsx'
 
 enableMSW().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
